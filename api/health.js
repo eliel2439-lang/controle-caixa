3c1471e38d14475ffd1ce9c0c11d9da6a0e507dd
eliel2439-lang <eliel2439@gmail.com>
2026-08-09 14:51:18 -0300

    Delete api directory

--- api/health.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    app: 'Gestão 360° Ultra',
-    service: 'api',
-    timestamp: new Date().toISOString()
-  });
-};
